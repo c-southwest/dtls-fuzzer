9028285584b067bd9e927eb5099eba3a261aba16
Oskar Pettersson Löfstedt <00oskpet@gmail.com>
2024-07-29 16:55:57 +0200

    SulRA, SulBuilderRA, MultiBuilderRA and a new main.

---
+++ src/main/java/se/uu/it/dtlsfuzzer/MainRA.java
@@ -0,0 +1,20 @@
+package se.uu.it.dtlsfuzzer;
+
+import com.github.protocolfuzzing.protocolstatefuzzer.entrypoints.CommandLineParser;
+import java.io.IOException;
+import java.security.Security;
+import javax.xml.stream.XMLStreamException;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+public class MainRA {
+
+    public static void main(String args[]) throws IOException, XMLStreamException {
+        Security.addProvider(new BouncyCastleProvider());
+        MultiBuilder mb = new MultiBuilder();
+        // String[] parentLoggers = {Main.class.getPackageName()};
+
+        CommandLineParser<?> commandLineParser = new CommandLineParser<>(mb, mb, mb, mb);
+        // commandLineParser.setExternalParentLoggers(parentLoggers);
+        commandLineParser.parse(args);
+    }
+}
